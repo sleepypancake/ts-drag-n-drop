1c53f3a1bb9e900420f26be7f37d95619dd88060
Anastasia Brikun <brikun.a@aicrobotics.ru>
2022-05-31 12:09:05 +0300

    feat: initializing input handler

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,20 @@
-// Code goes here!
+class ProjectInput {
+    templateElement: HTMLTemplateElement | null;
+    hostElement: HTMLDivElement | null;
+    element: HTMLFormElement;
+
+    constructor() {
+        this.templateElement = <HTMLTemplateElement>document.getElementById('project-input') || null;
+        this.hostElement = <HTMLDivElement>document.getElementById('app') || null; 
+
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = <HTMLFormElement>importedNode.firstElementChild;
+        this.attach();
+    }
+
+    private attach() {
+        this.hostElement?.insertAdjacentElement('afterbegin', this.element);
+    }
+}
+
+const prjInput = new ProjectInput()
